458b096c0498f3cd6722fe395abb804f8f600495
MINT <mint.2978@gmail.com>
2024-05-14 13:42:45 +0700

    feat: ex1-6

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,11 @@
 // Start coding here
-const minNumber = findMinNumber(10, 20);
 
+let findMinNumber = function (a, b) {
+  const resultMin = Math.min(a, b);
+  return resultMin;
+};
+
+const minNumber = findMinNumber(10, 20);
 console.log(minNumber); // 10
+
+console.log(Math.min(10, 20));
